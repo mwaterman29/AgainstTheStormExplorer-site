418fdf9c4025a382a23cea7eb6f05dfe3afd5d83
Matt Waterman <mwaterman29@gmail.com>
2024-01-10 21:59:20 -0500

    reagraph installed and sized

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import { GraphCanvas } from 'reagraph';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='bg-gray-200 text-green-300 h-screen w-screen'>
-      <p>test</p>
+      <div className='w-full h-[10%] bg-gray-400'>
+        test header
+      </div>
+      <div className='bg-purple-600 h-[80%] w-full p-2'>
+        <div className='relative h-full w-full'>
+          <GraphCanvas
+              nodes={[
+                {
+                  id: 'n-1',
+                  label: '1'
+                },
+                {
+                  id: 'n-2',
+                  label: '2'
+                }
+              ]}
+              edges={[
+                {
+                  id: '1->2',
+                  source: 'n-1',
+                  target: 'n-2',
+                  label: 'Edge 1-2'
+                }
+              ]}
+            />
+        </div>
+
+      </div>
+      <div className='h-[10%]'>
+
+      </div>
+
     </div>
   )
 }
