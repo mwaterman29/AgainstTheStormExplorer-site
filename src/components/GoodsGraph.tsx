b870868f02a11a57619a89abb3e2694f67746654
Matt Waterman <mwaterman29@gmail.com>
2024-01-18 16:02:41 -0500

    wip on custom edges

--- src/components/GoodsGraph.tsx
+++ src/components/GoodsGraph.tsx
@@ -1,4 +1,4 @@
-import { GraphCanvas, GraphCanvasRef, NodeRendererProps} from 'reagraph';
+import { GraphCanvas, GraphCanvasRef, NodeRendererProps, getCurve, getMidPoint, getVector, get } from 'reagraph';
 import { FunctionComponent, RefObject, useEffect, useState } from 'react';
 import * as THREE from 'three';
 import { useLoader } from '@react-three/fiber';
@@ -23,6 +23,10 @@ const GoodsGraph: FunctionComponent<{
     }, [selected, precursors, successors]);
     */
 
+    const nodePositions: {
+        [nodeId: string]: THREE.Vector3;
+    } = {};
+    
     return (
         <GraphCanvas
             ref={graphProps.graphRef}
@@ -37,8 +41,10 @@ const GoodsGraph: FunctionComponent<{
             edgeArrowPosition='mid'
             renderNode={(props: NodeRendererProps) => {
 
+                nodePositions[props.id] = getVector(props.node);
+
                 //console.log(graphProps.graphRef.current?.getGraph());
-                console.log(props.node.position.x, props.node.position.y);
+                //console.log(props.node.position.x, props.node.position.y);
 
                 const baseUrl = import.meta.env.BASE_URL;
                 const imageUri = baseUrl + 'icons/' + props.id + '.png';
@@ -49,13 +55,45 @@ const GoodsGraph: FunctionComponent<{
                 let color = new THREE.Color("#FFFFFF");
                 let selectionColor = new THREE.Color("#0000DD");
                 let precursorColor = new THREE.Color("#DD0000");
-                let successorColor = new THREE.Color("#00DD00");
+                let successorColor = new THREE.Color("#00DD00");    
+                
+                let precursor = graphProps.precursors?.includes(props.id);
 
+                //Prepare selection line
+                let precursorCurve;
+                if(precursor)
+                {
+                     precursorCurve = getCurve(
+                        nodePositions[props.id],//new THREE.Vector3(nodePositions[props.id].x, nodePositions[props.id][1], 0),
+                        5,
+                        nodePositions[graphProps.selected.id],//new THREE.Vector3(nodePositions[graphProps.selected.id][0], nodePositions[graphProps.selected.id][1], 0),
+                        5,
+                        false,
+                    );
+                    console.log("Precursor curve from " + props.id + " to " + graphProps.selected.id);
+                    console.log(precursorCurve)
+                }
                 
 
                 return (
                     <group>
-                        {graphProps.selected.id == props.node.id &&
+                        { // Lines
+                        }
+
+                        {precursor &&
+                            <mesh>
+                                <tubeGeometry attach="geometry" args={[precursorCurve, 20, 4, 5]}  />
+                                <meshBasicMaterial
+                                    attach="material"
+                                    color={precursorColor}
+                                />
+                            </mesh>
+                        }
+
+                        {//Selection highlighting 
+                        }
+
+                        {graphProps.selected.id == props.id &&
                             <mesh position={[0, 0, 5]}>
                                 <circleGeometry attach="geometry" args={[props.size + 3, 32]}  />
                                 <meshBasicMaterial
@@ -93,7 +131,7 @@ const GoodsGraph: FunctionComponent<{
                                 transparent
                             />
                         </mesh>
-                        
+                        {}
                   </group>
                 )
             }}
